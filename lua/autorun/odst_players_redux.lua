df22044e4fd109494cad01f42c7dd2a075218c57
BizLight <scrotebrub@gmail.com>
2019-01-29 20:42:29 +0200

    Add files via upload

---
+++ lua/autorun/odst_players_redux.lua
@@ -0,0 +1,98 @@
+
+-- MALES --
+player_manager.AddValidModel( "odst_redux_Alfie", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_alfie.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Alfie", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_alfie.mdl" )
+player_manager.AddValidHands("odst_redux_Alfie","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Carl", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_carl.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Carl", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_carl.mdl")
+player_manager.AddValidHands("odst_redux_Carl","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Donnie", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_donnie.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Donnie", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_donnie.mdl" )
+player_manager.AddValidHands("odst_redux_Donnie","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Fabrice", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_fabrice.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Fabrice", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_fabrice.mdl" )
+player_manager.AddValidHands("odst_redux_Fabrice","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Gilberto", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_gilberto.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Gilberto", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_gilberto.mdl" )
+player_manager.AddValidHands("odst_redux_Gilberto","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Islambek", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_islambek.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Islambek", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_islambek.mdl" )
+player_manager.AddValidHands("odst_redux_Islambek","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_John", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_john.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_John", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_john.mdl" )
+player_manager.AddValidHands("odst_redux_John","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Jose", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_jose.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Jose", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_jose.mdl" )
+player_manager.AddValidHands("odst_redux_Jose","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Michael", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_michael.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Michael", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_michael.mdl" )
+player_manager.AddValidHands("odst_redux_Michael","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Ray", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_ray.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Ray", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_ray.mdl" )
+player_manager.AddValidHands("odst_redux_Ray","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Rob", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_rob.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Rob", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_rob.mdl" )
+player_manager.AddValidHands("odst_redux_Rob","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Yasser", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_yasser.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Yasser", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_yasser.mdl" )
+player_manager.AddValidHands("odst_redux_Yasser","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Yohannes", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_yohannes.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Yohannes", "models/w4ys3r/halo_redux/odst/male/odst_redux_g_yohannes.mdl" )
+player_manager.AddValidHands("odst_redux_Yohannes","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Danlin", "models/w4ys3r/halo_redux/odst/male/odst_redux_danlin.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Danlin", "models/w4ys3r/halo_redux/odst/male/odst_redux_danlin.mdl" )
+player_manager.AddValidHands("odst_redux_Danlin","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Eyecberg", "models/w4ys3r/halo_redux/odst/male/odst_redux_eyecberg.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Eyecberg", "models/w4ys3r/halo_redux/odst/male/odst_redux_eyecberg.mdl" )
+player_manager.AddValidHands("odst_redux_Eyecberg","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Heretic", "models/w4ys3r/halo_redux/odst/male/odst_redux_heretic.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Heretic", "models/w4ys3r/halo_redux/odst/male/odst_redux_heretic.mdl" )
+player_manager.AddValidHands("odst_redux_Heretic","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Jeffrey", "models/w4ys3r/halo_redux/odst/male/odst_redux_jeffrey.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Jeffrey", "models/w4ys3r/halo_redux/odst/male/odst_redux_jeffrey.mdl" )
+player_manager.AddValidHands("odst_redux_Jeffrey","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Snippy", "models/w4ys3r/halo_redux/odst/male/odst_redux_snippy.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Snippy", "models/w4ys3r/halo_redux/odst/male/odst_redux_snippy.mdl" )
+player_manager.AddValidHands("odst_redux_Snippy","models/w4ys3r/halo_redux/odst/male/odst_redux_m_arms.mdl",0,"00000000")
+
+
+
+
+-- FEMALES --
+player_manager.AddValidModel( "odst_redux_Bella", "models/w4ys3r/halo_redux/odst/female/odst_redux_bella.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Bella", "models/w4ys3r/halo_redux/odst/female/odst_redux_bella.mdl" )
+player_manager.AddValidHands("odst_redux_Bella","models/w4ys3r/halo_redux/odst/female/odst_redux_f_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Katya", "models/w4ys3r/halo_redux/odst/female/odst_redux_katya.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Katya", "models/w4ys3r/halo_redux/odst/female/odst_redux_katya.mdl" )
+player_manager.AddValidHands("odst_redux_Katya","models/w4ys3r/halo_redux/odst/female/odst_redux_f_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Mira", "models/w4ys3r/halo_redux/odst/female/odst_redux_mira.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Mira", "models/w4ys3r/halo_redux/odst/female/odst_redux_mira.mdl" )
+player_manager.AddValidHands("odst_redux_Mira","models/w4ys3r/halo_redux/odst/female/odst_redux_f_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Neca", "models/w4ys3r/halo_redux/odst/female/odst_redux_neca.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Neca", "models/w4ys3r/halo_redux/odst/female/odst_redux_neca.mdl" )
+player_manager.AddValidHands("odst_redux_Neca","models/w4ys3r/halo_redux/odst/female/odst_redux_f_arms.mdl",0,"00000000")
+
+player_manager.AddValidModel( "odst_redux_Reid", "models/w4ys3r/halo_redux/odst/female/odst_redux_reid.mdl" )
+list.Set( "PlayerOptionsModel","odst_redux_Reid", "models/w4ys3r/halo_redux/odst/female/odst_redux_reid.mdl" )
+player_manager.AddValidHands("odst_redux_Reid","models/w4ys3r/halo_redux/odst/female/odst_redux_f_arms.mdl",0,"00000000")
+
